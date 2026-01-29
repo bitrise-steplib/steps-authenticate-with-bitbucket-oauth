bbd0844302bac08125562fa068f0ad8aafcf9056
Olivér Falvai <ofalvai@gmail.com>
2026-01-29 15:44:21 +0100

    Migrate go-utils and go-steputils usage to v2

--- main.go
+++ main.go
@@ -1,111 +1,92 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"os"
 	"strings"
 	"time"
 
-	"github.com/bitrise-io/depman/pathutil"
-	"github.com/bitrise-io/go-utils/fileutil"
-	"github.com/bitrise-io/go-utils/log"
+	"github.com/bitrise-io/go-steputils/v2/stepconf"
+	"github.com/bitrise-io/go-utils/v2/pathutil"
+	"github.com/bitrise-io/go-utils/v2/env"
+	"github.com/bitrise-io/go-utils/v2/fileutil"
+	"github.com/bitrise-io/go-utils/v2/log"
 	"github.com/bitrise-steplib/steps-authenticate-host-with-netrc/netrcutil"
-	"github.com/bitrise-tools/go-steputils/input"
 )
 
-// ConfigsModel ...
 type ConfigsModel struct {
 	BitbucketHosts []string
-	Username       string
-	AccessToken    string
-}
-
-func createConfigsModelFromEnvs() ConfigsModel {
-	return ConfigsModel{
-		BitbucketHosts: []string{"bitbucket.org", "api.bitbucket.org"},
-		Username:       os.Getenv("username"),
-		AccessToken:    os.Getenv("access_token"),
-	}
-}
-
-func (configs *ConfigsModel) validate() error {
-	if err := input.ValidateIfNotEmpty(configs.Username); err != nil {
-		return errors.New("Username, parameter not specified")
-	}
-	if err := input.ValidateIfNotEmpty(configs.AccessToken); err != nil {
-		return errors.New("AppPassword, parameter not specified")
-	}
-	return nil
-}
-
-func (configs *ConfigsModel) print() {
-	log.Infof("Configs:")
-	log.Printf("- Username: %s", configs.Username)
-	log.Printf("- AppPassword: %s", input.SecureInput(configs.AccessToken))
-}
-
-func failf(message string, args ...interface{}) {
-	log.Errorf(message, args...)
-	os.Exit(1)
+	Username       string          `env:"username,required"`
+	AccessToken    stepconf.Secret `env:"access_token,required"`
 }
 
 func main() {
-	configs := createConfigsModelFromEnvs()
+	logger := log.NewLogger()
+	fileManager := fileutil.NewFileManager()
 
-	configs.print()
-	fmt.Println()
+	failf := func(message string, args ...interface{}) {
+		logger.Errorf(message, args...)
+		os.Exit(1)
+	}
 
-	if err := configs.validate(); err != nil {
+	var configs ConfigsModel
+	configs.BitbucketHosts = []string{"bitbucket.org", "api.bitbucket.org"}
+
+	parser := stepconf.NewInputParser(env.NewRepository())
+	if err := parser.Parse(&configs); err != nil {
 		failf("Issue with input: %s", err)
 	}
 
+	stepconf.Print(configs)
+	fmt.Println()
+
 	netRC := netrcutil.New()
 
-	log.Infof("Other configs:")
-	log.Printf("- OutputPath: %s", netRC.OutputPth)
+	logger.Infof("Other configs:")
+	logger.Printf("- OutputPath: %s", netRC.OutputPth)
 
 	fmt.Println()
 
-	log.Infof("Adding host config...")
+	logger.Infof("Adding host config...")
 	for _, host := range configs.BitbucketHosts {
-		netRC.AddItemModel(netrcutil.NetRCItemModel{Machine: host, Login: configs.Username, Password: configs.AccessToken})
-		log.Printf("- Added: %s", host)
+		netRC.AddItemModel(netrcutil.NetRCItemModel{Machine: host, Login: configs.Username, Password: string(configs.AccessToken)})
+		logger.Printf("- Added: %s", host)
 	}
 
 	fmt.Println()
 
-	log.Infof("Writing .netrc file...")
+	logger.Infof("Writing .netrc file...")
 
-	isExists, err := pathutil.IsPathExists(netRC.OutputPth)
+	isExists, err := pathutil.NewPathChecker().IsPathExists(netRC.OutputPth)
 	if err != nil {
 		failf("Failed to check path (%s), error: %s", netRC.OutputPth, err)
 	}
 
 	if !isExists {
-		log.Printf("No .netrc file found at (%s), creating new...", netRC.OutputPth)
+		logger.Printf("No .netrc file found at (%s), creating new...", netRC.OutputPth)
 
 		if err := netRC.CreateFile(); err != nil {
 			failf("Failed to write .netrc file, error: %s", err)
 		}
 	} else {
-		log.Warnf("File already exists at (%s)", netRC.OutputPth)
+		logger.Warnf("File already exists at (%s)", netRC.OutputPth)
 
 		backupPth := fmt.Sprintf("%s%s", strings.Replace(netRC.OutputPth, ".netrc", ".bk.netrc", -1), time.Now().Format("2006_01_02_15_04_05"))
 
-		if originalContent, err := fileutil.ReadBytesFromFile(netRC.OutputPth); err != nil {
+		originalContent, err := os.ReadFile(netRC.OutputPth)
+		if err != nil {
 			failf("Failed to read file (%s), error: %s", netRC.OutputPth, err)
-		} else if err := fileutil.WriteBytesToFile(backupPth, originalContent); err != nil {
-			failf("Failed to write file (%s), error: %s", backupPth, err)
-		} else {
-			log.Printf("Backup created at: %s", backupPth)
 		}
+		if err := fileManager.WriteBytes(backupPth, originalContent); err != nil {
+			failf("Failed to write file (%s), error: %s", backupPth, err)
+		}
+		logger.Printf("Backup created at: %s", backupPth)
 
-		log.Printf("Appending config to the existing .netrc file...")
+		logger.Printf("Appending config to the existing .netrc file...")
 
 		if err := netRC.Append(); err != nil {
 			failf("Failed to write .netrc file, error: %s", err)
 		}
 	}
-	log.Donef("Success")
+	logger.Donef("Success")
 }
